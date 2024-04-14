c8df9f030ecde72504fe8100600d8ff366d7c663
Andrew Napitupulu <angularsprings403@gmail.com>
2024-04-14 23:59:46 +0700

    race condition

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package belajar_golang_goroutines
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -54,4 +55,66 @@ func TestInOutChannel(t *testing.T) {
 	go OnlyOut(channel)
 
 	time.Sleep(5 * time.Second)
+}
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "andrew"
+		channel <- "wiliam"
+		channel <- "napitupulu"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan Ke- " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima Data", data)
+	}
+	fmt.Println("Selesai")
+
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2 ", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
 }
